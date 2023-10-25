54ead4db04c9ecddfd507d2ff8539b943aceac43
bikramia <bikram.rhce@gmail.com>
2023-10-25 09:08:20 -0400

    third

---
+++ project_assignment/vgc.py
@@ -0,0 +1,15 @@
+class Video_Game_Character:
+    """"A class to represent a video game charater."""
+
+    def __init__():
+        """"Initializing a new VideoGameCharater object
+        
+        Args:
+            Character_type: The type of character(e.g agre, human, elf).
+            health_points: The character's current health points.
+            attack_points: The character's attack points.
+            weapons: A list or dictionary of the character's weapons.
+        """
+        self.character_type = 
+
+        
